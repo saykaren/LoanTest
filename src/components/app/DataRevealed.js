185e379d064006b6d3cf8760bb8f471521af5583
Karen Garcia <kgarcia@i3screen.com>
2020-04-20 15:47:09 -0600

    Added SCSS and loan code

---
+++ src/components/app/DataRevealed.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import numberConverter from './numberConverter';
+
+const RevealData = ({interestPaidArray, mortgage})=>{
+
+    let paidInAss;
+
+
+    // const arrayResults = interestPaidArray;
+    if(interestPaidArray.length > 0){
+        paidInAss = numberConverter(interestPaidArray.reduce((accu, cur)=>(
+
+            accu + cur
+        )))
+
+    }
+    const totalPaid = paidInAss + parseInt(mortgage);
+
+    return(
+
+        <section >
+            {interestPaidArray.length>0 && <div className="dataForm">
+                <div className="dataSection">
+                    You Paid the Bank ${paidInAss}
+                </div>
+                <div className="dataSection">
+                    Total you paid ${totalPaid} for a loan of ${mortgage}
+                </div>
+                <div className="bar dataSection" >
+                    <div id="principalBar" style={{width: `{${mortgage}/${totalPaid}}%`, backgroundColor: '#282c34', border: '4px solid white'}}>
+                        ${mortgage} Mortgage
+                    </div>
+                    <div id="interestPaid" style={{width: `{${paidInAss}/${totalPaid}}%`, backgroundColor: '#61dafb', border: '4px solid white', color: '#282c34' }}>
+                        ${paidInAss} Interest Paid
+                    </div>
+                </div>
+                <div className="dataSection">
+                    Will take you {interestPaidArray.length} months to pay off this loan
+                </div>
+            </div>}
+        </section>
+    )
+}
+
+export default RevealData;
---
+++ src/components/app/numberConverter.js
@@ -0,0 +1,5 @@
+const numberConverter =(value) =>{
+    return parseFloat((value).toFixed(2));
+};
+
+export default numberConverter;
